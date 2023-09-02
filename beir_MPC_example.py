ccee0cd78d85c79f7221ea3007c51147d8492b54
tobinsouth <tobin.south@gmail.com>
2023-09-02 17:16:09 -0400

    Making the MPC more friendly

--- beir_MPC_example.py
+++ beir_MPC_example.py
@@ -7,7 +7,6 @@ from beir.retrieval.evaluation import EvaluateRetrieval
 from LocalDenseRetrievalExactSearch import DenseRetrievalExactSearch
 from MPCDenseRetrievalExactSearch import MPCDenseRetrievalExactSearch
 
-
 import logging
 import pathlib, os, numpy as np
 import random, pickle
@@ -15,32 +14,6 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO,
                     handlers=[LoggingHandler()])
-
-# Setup
-dataset = "trec-covid"
-url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
-out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "datasets")
-data_path = util.download_and_unzip(url, out_dir)
-corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
-
-# We're going to trim down the dataset for testing.
-
-def reduce_corpus_size(corpus, qrels, queries, sample_size: int = 500):
-    sample_size = 500
-    queries = {k:v for k,v in queries.items() if np.random.rand() < 0.05}
-    corpus_ids, query_ids = list(corpus), list(queries)
-    qrels = {k:v for k,v in qrels.items() if k in query_ids}
-    corpus_set = set()
-    for query_id in qrels:
-        corpus_set.update(list(qrels[query_id].keys()))
-    corpus_new = {corpus_id: corpus[corpus_id] for corpus_id in corpus_set}
-
-    remaining_corpus = list(set(corpus_ids) - corpus_set)
-
-    for corpus_id in random.sample(remaining_corpus, sample_size):
-        corpus_new[corpus_id] = corpus[corpus_id]
-    corpus = corpus_new
-    return corpus, qrels, queries
 
 def benchmark_retriever(retriever, corpus, queries, qrels):
     start_time = time()
@@ -56,32 +29,65 @@ def benchmark_retriever(retriever, corpus, queries, qrels):
     print("Performance of DenseRetrievalExactSearch: {recall}, {precision}, {ndcg}, {map}, {mrr}, {recall_cap}, {hole}".format(recall=recall, precision=precision, ndcg=ndcg, map=_map, mrr=mrr, recall_cap=recall_cap, hole=hole))
     return recall, precision
 
+def setup(reduce_corpus_size: bool = True):
+    """This is a good one-time function to run to start embedding the corpus and queries and then save them to file for later use."""
+    # Setup
+    dataset = "trec-covid"
+    url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
+    out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "datasets")
+    data_path = util.download_and_unzip(url, out_dir)
+    corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
 
-corpus, qrels, queries = reduce_corpus_size(corpus, qrels, queries, 500)
-print("Corpus size: {} on {} queries".format(len(corpus), len(queries)))
-# It's good to save these for reproducibility
-pickle.dump([corpus, qrels, queries], open("corpus.pkl", "wb"))
+    # We're going to trim down the dataset for testing.
+
+    def reduce_corpus_size(corpus, qrels, queries, sample_size: int = 500, proportion: float = 0.05):
+        sample_size = 500
+        queries = {k:v for k,v in queries.items() if np.random.rand() < proportion}
+        corpus_ids, query_ids = list(corpus), list(queries)
+        qrels = {k:v for k,v in qrels.items() if k in query_ids}
+        corpus_set = set()
+        for query_id in qrels:
+            corpus_set.update(list(qrels[query_id].keys()))
+        corpus_new = {corpus_id: corpus[corpus_id] for corpus_id in corpus_set}
+
+        remaining_corpus = list(set(corpus_ids) - corpus_set)
+
+        for corpus_id in random.sample(remaining_corpus, sample_size):
+            corpus_new[corpus_id] = corpus[corpus_id]
+        corpus = corpus_new
+        return corpus, qrels, queries
+    
+    if reduce_corpus_size:
+        corpus, qrels, queries = reduce_corpus_size(corpus, qrels, queries, 500, 0.05)
+    print("Corpus size: {} on {} queries".format(len(corpus), len(queries)))
+    # It's good to save these for reproducibility
+    pickle.dump([corpus, qrels, queries], open("corpus.pkl", "wb"))
+
+    #### Dense Retrieval using SBERT (Sentence-BERT) ####
+    print("Beginning embedding")
+    embedding_model = models.SentenceBERT("msmarco-distilbert-base-tas-b")
+    from sentence_transformers import SentenceTransformer
+    embedding_model.q_model = SentenceTransformer("msmarco-distilbert-base-tas-b", device="mps") # This is just to force the pytorch device for speed reasons
+
+    model = DenseRetrievalExactSearch(embedding_model, batch_size=256, corpus_chunk_size=512*9999)
+
+    model.preemebed_corpus(corpus, save_path="corpus_embeddings.pt")
+    model.preembed_queries(queries, save_path="query_embeddings.pt")
+
+    print("Finished embedding, testing out retrieval")
+    # Now we benchmark normal dense retrieval
+    retriever = EvaluateRetrieval(model, score_function="cos_sim")
+    benchmark_retriever(retriever, corpus, queries, qrels)
+
+
 corpus, qrels, queries = pickle.load(open("corpus.pkl", "rb"))
 
 
-#### Dense Retrieval using SBERT (Sentence-BERT) ####
-embedding_model = models.SentenceBERT("msmarco-distilbert-base-tas-b")
-from sentence_transformers import SentenceTransformer
-embedding_model.q_model = SentenceTransformer("msmarco-distilbert-base-tas-b", device="mps") # This is just to force the pytorch device for speed reasons
-
-model = DenseRetrievalExactSearch(embedding_model, batch_size=256, corpus_chunk_size=512*9999)
-
-# model.preemebed_corpus(corpus, save_path="corpus_embeddings.pt")
-# model.preembed_queries(queries, save_path="query_embeddings.pt")
-model.load_preembeddings("corpus_embeddings.pt", "query_embeddings.pt")
-
-# Now we benchmark normal dense retrieval
-retriever = EvaluateRetrieval(model, score_function="cos_sim")
-benchmark_retriever(retriever, corpus, queries, qrels)
-
-
 # Now we benchmark MPC dense retrieval
+embedding_model = models.SentenceBERT("msmarco-distilbert-base-tas-b")
 model = MPCDenseRetrievalExactSearch(embedding_model, batch_size=256, corpus_chunk_size=512*9999)
+
+# Load in premade embeddings
 model.load_preembeddings("corpus_embeddings.pt", "query_embeddings.pt")
 
 retriever = EvaluateRetrieval(model, score_function="cos_sim")
